57fd8810e40e69e1e7530bc298354b0aaf5d820a
sondo65 <sondo65@gmail.com>
2020-05-11 20:08:29 +0700

    init project

---
+++ app/src/main/java/sondo65/com/lazada/MainActivity.kt
@@ -0,0 +1,12 @@
+package sondo65.com.lazada
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
